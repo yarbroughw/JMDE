b6f297638c6fae49679819ed7766656837fcaa79
Willem Yarbrough <wyarbrough93@gmail.com>
2015-03-18 13:20:25 -0400

    TreeClassifier implementation near completion

--- ontologyproj/treeclassifier.py
+++ ontologyproj/treeclassifier.py
@@ -1,46 +1,99 @@
+import json
+import retrieve
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
 class Node:
-    def __init__(self,n,classifier):
-        self.name       = n
-        self.children   = []
-        self.classifier = classifier
+    def __init__(self,n,pipeline):
+        self.name     = n
+        self.children = []
+        self.pipeline = pipeline
 
     def hasChildren(self):
         return self.children != []
 
-    def train(trainnum,testnum):
-        trainset,testset = retrieve.sets(category.name)
-        corpus = list(map(getkeystring,  trainset))
-        target = list(map(getclasslabel, testset))
-        self.classifier = pipeline().fit(corpus,target)
+    def getsets(self,trainnum,testnum):
+        trainset,testset = retrieve.sets(self.name,trainnum,testnum)
+        self.trainset = trainset
+        self.testset  = testset
 
-    def condfidence(self,entity):
-        # TODO
-        pass
+    def train(self):
+        if not self.trainset:
+            print(self.name + "'s training set not initialized!")
+            return
+        corpus = features(self.trainset)
+        target = labels(self.trainset)
+        self.pipeline = self.pipeline.fit(corpus,target)
 
+    def test(self):
+        if not self.testset:
+            print(self.name + "'s test set not initialized!")
+            return
+        corpus = features(self.testset)
+        target = labels(self.testset)
+        return self.pipeline.score(corpus,target)
+
+    def confidence(self,entity):
+        vector = getkeystring(entity)
+        return self.pipeline.predict_proba(vector)
 
 class TreeClassifier:
-    def __init__(self):
-        # make tree from nested ontology
+    def __init__(self,subclass=None):
+        self.pipeline  = pipeline()
         self.threshold = 0.05
 
-    def train(trainnum,testnum):
+        # make tree from nested ontology
+        with open("../data/nestedontology.json",'r') as f:
+            ontology = json.load(f)[0]
+        self.root = self._buildtree(ontology)
+        if subclass: self.root = self.getsubnode(subclass)
+
+    def getsubnode(self,classname):
+        ''' returns the node in the tree that matches classname '''
+        for node in iter(self):
+            if node.name == classname:
+                return node
+        raise ValueError(classname + " is not a valid class name!")
+
+    def _buildtree(self,json_tree):
+        ''' build tree from nested json '''
+        root = Node(json_tree["name"],self.pipeline)
+        for child in json_tree["children"]:
+            root.children.append(self._buildtree(child))
+        return root
+
+    def __iter__(self):
+        ''' BFS traversal of tree '''
+        queue = [self.root]
+        while queue != []:
+            current = queue.pop(0)
+            queue.extend(current.children)
+            yield current
+
+    def train(self,trainnum,testnum):
         ''' train each node's classifier '''
         for node in iter(self):
-            node.train(trainnum,testnum)
+            print("Training",node.name)
+            node.getsets(trainnum,testnum)
+            node.train()
 
-    def predict(entity):
+    def predict(self,entity):
         ''' predict downwards in tree from root node '''
-        #TODO: flesh out
         node = self.root
-        # predict down in tree
         while node.hasChildren() and node.confidence(entity) > self.threshold:
             node = node.predict(entity)
+        return node.name
 
-def getkeystring(entity):
-    return ' '.join(entity["properties"])
+def features(dataset):
+    getkeystring = lambda x: ' '.join(x["properties"])
+    return list(map(getkeystring, dataset))
 
-def getclasslabel(entity):
-    return entity["class"]
+def labels(dataset):
+    getclassname = lambda x: x["class"]
+    return list(map(getclassname, dataset))
 
 def pipeline():
     return Pipeline([('vect',CountVectorizer()),
@@ -48,3 +101,5 @@ def pipeline():
                      ('clf',MultinomialNB()),
                    ])
 
+if __name__ == "__main__":
+    tree = TreeClassifier()
