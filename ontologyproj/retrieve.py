998b7e3f9e9565f604ee655b6c5c7ff4e3de533c
Willem Yarbrough <wyarbrough93@gmail.com>
2015-03-19 19:03:23 -0400

    flake8 edits to retrieve.py

--- ontologyproj/retrieve.py
+++ ontologyproj/retrieve.py
@@ -1,23 +1,23 @@
-import sys
 import random
 import ujson
+
 
 def entitygenerator(category):
     ''' generator using directory file to yield random json from class '''
 
     # get path of category from ontology file
-    with open("../data/flatontology.json",'r') as ontology:
+    with open("../data/flatontology.json", 'r') as ontology:
         category_path = ujson.load(ontology)[category]["fullpath"]
 
     # get child names of category
-    with open("../data/nestedontology.json",'r') as ontology:
+    with open("../data/nestedontology.json", 'r') as ontology:
         tree = ujson.load(ontology)[0]
 
-    childnames = getchildnames(tree,category_path)
+    childnames = getchildnames(tree, category_path)
 
     # load index file from that path
     indexfile = "../mockdata/" + category_path + "fileindex.json"
-    with open(indexfile,'r') as directory:
+    with open(indexfile, 'r') as directory:
         filedict = ujson.loads(directory.read())
 
     # pick random entry in index and return associated object
@@ -33,20 +33,24 @@ def entitygenerator(category):
         entity["class"] = (childnames & classes).pop()
         yield entity
 
-def getchildnames(tree,path):
+
+def getchildnames(tree, path):
     ''' returns names of direct children of category in tree '''
     path = path.split('/')[:-1]
     node = tree
     while path != []:
         level = path.pop(0)
         for child in node["children"]:
-            if child["name"] == level: node = child
-    return set([ child["name"] for child in node["children"] ])
+            if child["name"] == level:
+                node = child
+    return set([child["name"] for child in node["children"]])
 
-def progress(counter,total):
+
+def progress(counter, total):
     ''' simple helper function for string of progress '''
     percentage = int(counter / total * 100)
     return '\r'+str(percentage)+"%"
+
 
 def entities(amount, category):
     ''' helper function that wraps entity generator '''
@@ -54,18 +58,19 @@ def entities(amount, category):
     for _ in range(amount):
         yield next(g)
 
-def sets(category,trainnum,testnum):
+
+def sets(category, trainnum, testnum):
     ''' pull random training and testing sets from disk '''
     g = entitygenerator(category)
     trainset = []
-    testset  = []
+    testset = []
 
     # built trainset while printing out progress percentage
     counter = 0
     for _ in range(trainnum):
         trainset.append(next(g))
         counter += 1
-        print(progress(counter,trainnum),"of training set loaded",end='')
+        print(progress(counter, trainnum), "of training set loaded", end='')
     print()
 
     # built testset while printing out progress percentage
@@ -73,6 +78,6 @@ def sets(category,trainnum,testnum):
     for _ in range(testnum):
         testset.append(next(g))
         counter += 1
-        print(progress(counter,testnum),"of test set loaded",end='')
+        print(progress(counter, testnum), "of test set loaded", end='')
 
     return trainset, testset
