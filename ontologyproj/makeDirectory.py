6cef4c268418b5e3627dd41492b07bf28f5d82b5
Willem Yarbrough <wyarbrough93@gmail.com>
2015-03-20 19:26:03 -0400

    style edits to makedirectory script

--- ontologyproj/makeDirectory.py
+++ ontologyproj/makeDirectory.py
@@ -1,20 +1,22 @@
 ''' script for making directory structure of dbpedia ontology '''
 
+from __future__ import print_function
 import json
 import os
+
 
 def makeDirectories(ontologyDict):
     for value in ontologyDict:
         directory = "../mockdata/" + value["fullpath"]
-        print "making directory",directory," ...",
+        print("making directory", directory, " ...", end='')
         os.mkdir(directory)
-        with open(directory+"/fileindex.json",'a') as indexfile:
+        with open(directory+"/fileindex.json", 'a') as indexfile:
             d = {}
-            json.dump(d,indexfile)
-        print "done."
+            json.dump(d, indexfile)
+        print("done.")
         makeDirectories(value["children"])
 
-with open("../data/nestedontology.json",'r') as ontologyFile:
+with open("../data/nestedontology.json", 'r') as ontologyFile:
     ontology = json.load(ontologyFile)
 
 makeDirectories(ontology)
