4853ac50523699178ee5ce7aa50ef0a8aa3e5f1f
Willem Yarbrough <wyarbrough93@gmail.com>
2015-03-20 19:24:04 -0400

    unpackclass now uses CSV files only

--- ontologyproj/unpackClass.py
+++ ontologyproj/unpackClass.py
@@ -1,16 +1,15 @@
-''' takes dbpedia class file (a giant JSON file)
-    and splits it into separate, cleaned entity files
+''' takes dbpedia CSV class file and splits it
+    into separate, cleaned entity files
 '''
 
 from __future__ import print_function
-import ijson
 import json
+import csv
+from itertools import count
 
-ontologyprefix = "http:\/\/dbpedia.org\/ontology\/"
-ontologyprefix2 = "http://dbpedia.org/ontology/"
-resourceprefix = "http:\/\/dbpedia.org\/resource\/"
-itemprefix = "http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#type"
-itemprefix2 = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+ontologyprefix = "http://dbpedia.org/ontology/"
+resourceprefix = "http://dbpedia.org/resource/"
+itemprefix = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 with open("../data/flatontology.json", 'r') as flat:
     ontology = json.load(flat)
 
@@ -19,33 +18,52 @@ def toName(url):
     return url[len(ontologyprefix):]
 
 
-def toName2(url):
-    return url[len(ontologyprefix2):]
+def csvinstances(path, skip=1):
+    with open(path, 'r') as classfile:
+        dicts = csv.DictReader(classfile)
+        labelURIs = next(dicts)     # field label URIs
+        next(dicts)     # field types      (unneeded)
+        next(dicts)     # field type URIs  (unneeded)
+        for i, instance in zip(count(), dicts):
+            if i % skip == 0:
+                yield cleanCSVinstance(instance, labelURIs)
 
 
-def splitClassFile(classfilepath, destination, skip=1):
+def cleanCSVinstance(instance, labeldict):
+    tolist = lambda x: x[1:-1].split('|')
+
+    newinstance = {"properties": {}}
+    for key, value in instance.items():
+        if labeldict[key].startswith(ontologyprefix):
+            newinstance["properties"][key] = value
+            if value[0] == '{':
+                newinstance["properties"][key] = tolist(value)
+
+    typelabel = "22-rdf-syntax-ns#type"
+    newinstance[typelabel] = tolist(instance[typelabel])
+    newinstance["name"] = instance["rdf-schema#label"]
+    newinstance["URI"] = instance["URI"]
+    return newinstance
+
+
+def writeInstances(instances, dest):
     ''' stream of all instances in file '''
-    classfile = open(classfilepath, 'r')
-    i = 0
-    for instance in ijson.items(classfile, 'instances.item'):
-        if i == 0:
-            toEntity(instance, destination)
-        i = (i + 1) % skip
-    classfile.close()
+    for instance in instances:
+        writeEntity(instance, dest)
 
 
-def toEntity(instance, dest):
+def writeEntity(instance, dest):
     entity = buildEntity(instance)
     if len(entity['properties']) > 2:
         deepest = getFinestOntology(entity['ontologies'])
-        path = ontology[toName2(deepest)]['fullpath']
+        path = ontology[toName(deepest)]['fullpath']
         write(entity, dest, path)
         addToIndexes(entity, dest, path)
 
 
 def buildEntity(instance):
-    return {'url': instance.keys()[0].replace('\\', ''),
-            'name': instance.keys()[0][len(resourceprefix):],
+    return {'URI': instance["URI"],
+            'name': instance["name"],
             'properties': getProperties(instance),
             'ontologies': getOntologies(instance)}
 
@@ -53,37 +71,39 @@ def buildEntity(instance):
 def getProperties(instance):
     ''' returns dict of all properties in instance '''
 
-    validproperty = lambda key, val: key.startswith(ontologyprefix) \
-        and not key.endswith("_label") \
-        and key != ontologyprefix + "wikiPageRevisionID" \
-        and key != ontologyprefix + "wikiPageID" \
+    validproperty = lambda key, val: not key.endswith("_label") \
+        and key != "wikiPageRevisionID" \
+        and key != "wikiPageID" \
+        and key != "wikiPageRedirects" \
+        and key != "22-rdf-syntax-ns#type" \
+        and key != "thumbnail" \
         and val != "NULL"
 
-    allprops = instance.values()[0]
-    properties = {toName(key): val for key, val in allprops.iteritems()
+    allprops = instance["properties"]
+    properties = {key: val for key, val in allprops.items()
                   if validproperty(key, val)}
     return properties
 
 
 def getOntologies(instance):
     ''' construct list of ontology refs in instance '''
-    ontologyrefs = instance.values()[0][itemprefix]
-    return [x for x in ontologyrefs if x.startswith(ontologyprefix2)]
+    ontologyrefs = instance["22-rdf-syntax-ns#type"]
+    return [x for x in ontologyrefs if x.startswith(ontologyprefix)]
 
 
 def getFinestOntology(refs):
     ''' take list of ontology classes and return deepest one '''
-    getdepth = lambda ref: ontology[toName2(ref)]['depth']
+    getdepth = lambda ref: ontology[toName(ref)]['depth']
     return max(refs, key=getdepth)
 
 
 def write(entity, dest, path):
     ''' write entity to JSON file at path '''
-    name = entity['name'].replace('\/', '-')
+    name = entity['name']
     fullname = dest + path + name + ".json"
-    with open(fullname,  'wb') as fp:
-        json.dump(entity,  fp)
-    print("wrote", entity['name'], "...", end='')
+    with open(fullname, 'w') as fp:
+        json.dump(entity, fp)
+    print("wrote", name, "...", end='')
 
 
 def addToIndexes(entity, dest, path):
@@ -96,8 +116,8 @@ def addToIndexes(entity, dest, path):
 
 def addToIndex(entity, dest, path):
     ''' add name and filepath to file index at dest '''
-    this_entity = entity['url']
-    val = path + entity['name'].replace('/', '-') + ".json"
+    this_entity = entity['name']
+    val = path + entity['name'] + ".json"
 
     with open(dest+"/fileindex.json", 'r') as f:
         index = json.load(f)
@@ -106,10 +126,6 @@ def addToIndex(entity, dest, path):
         json.dump(index,  f,  indent=4)
 
 
-def main():
-    speciespath = "../data/Species.json"
-    splitClassFile(speciespath, "../mockdata/", skip=10)
-
-
 if __name__ == "__main__":
-    main()
+    instances = csvinstances("/Users/will/Desktop/Brewery.csv")
+    writeInstances(instances, "../mockdata/")
