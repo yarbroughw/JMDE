6b405e47a0eec14f3069837e33e86a2a53d683a8
Willem Yarbrough <wyarbrough93@gmail.com>
2015-03-30 12:56:20 -0400

    bugfixes and refactoring to lexicalmatcher.py

--- ontologyproj/lexicalmatcher.py
+++ ontologyproj/lexicalmatcher.py
@@ -3,18 +3,21 @@ import retrieve
 
 
 def distance(inputprops, targetprops):
+    if not targetprops:
+        return 0
     matches = [x for x in inputprops if x in targetprops]
     return (len(matches) / len(targetprops))
 
 
 class LexicalMatcher:
     def __init__(self):
-        with open("../data/allproperties.json") as f:
+        with open("../data/properties.json") as f:
             self.properties = json.load(f)
 
     def closestclass(self, properties):
         maxdist = 0
-        for k, v in self.properties:
+        bestclass = None
+        for k, v in self.properties.items():
             if distance(properties, v) > maxdist:
                 bestclass = k
         return bestclass
@@ -26,15 +29,18 @@ class LexicalMatcher:
 
 if __name__ == "__main__":
 
+    entitycount = 1000
+
     matcher = LexicalMatcher()
-    entities = retrieve.entities(100, "owl:Thing")
+    entities = retrieve.entities(entitycount, "owl:Thing")
     total = 0
 
     for entity in entities:
         props = entity["properties"]
+        print("checking", entity["name"])
         guessed = matcher.classify(props)
-        if guessed == entity["Class"]:
+        if guessed == entity["class"]:
             total += 1
 
-    correctratio = total / len(entities)
+    correctratio = total / entitycount
     print(correctratio, "of the entities guessed correctly.")
