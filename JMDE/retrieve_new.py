97236b240dce3172dc7c673dd6ffe5acb8fd314c
Willem Yarbrough <wyarbrough93@gmail.com>
2015-08-14 11:28:52 -0700

    removed properties function in favor of preprocessed "compressed" dataset

--- JMDE/retrieve_new.py
+++ JMDE/retrieve_new.py
@@ -7,5 +7,10 @@ def instances():
     with open("../data/instances.json") as f:
         return json.load(f)
 
-def properties(instance):
-    return instance["properties"].keys()
+def compressed_instances():
+    ''' Loads and returns dataset of instances, where the instances
+    have been simplified to just property labels and class name.
+    See the scripts/repackage.py for more details.
+    '''
+    with open("../data/compressed_instances.json") as f:
+        return json.load(f)
